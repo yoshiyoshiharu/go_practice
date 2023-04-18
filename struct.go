7ccdc0b5f87fbe5d69d152ab5a55fae9c5f2dcb1
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2023-04-18 21:23:51 +0900

    add struct.go

---
+++ struct.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age int
+	Birthday string
+}
+
+// クラスメソッドに相当する関数は関数名の前に (thisに相当する変数 *構造体名)
+func (p *Person) setPerson(name string, age int, birthday string) {
+	p.name = name
+	p.age = age
+	p.Birthday = birthday
+}
+
+func (p *Person) getPerson() (string, int, string) {
+	return p.name, p.age, p.Birthday
+}
+
+func main() {
+  var p1 Person
+
+	p1.setPerson("Taro", 20, "1990/01/01")
+
+	fmt.Println(p1.name, p1.age, p1.Birthday)
+}
+
