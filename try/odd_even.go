e581aab6855058541f8154569f9dee119e37a7e2
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2023-04-20 21:34:18 +0900

    odd even

---
+++ try/odd_even.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	for num := 1; num <= 100; num++ {
+		if num % 2 == 0 {
+			fmt.Printf("%d-%s\n", num, "偶数")
+		} else {
+			fmt.Printf("%d-%s\n", num, "奇数")
+		}
+	}
+
+	// switchで書く
+	for num := 1; num <= 100; num++ {
+		switch {
+		case num % 2 == 0:
+			fmt.Printf("%d-%s\n", num, "偶数")
+		default:
+			fmt.Printf("%d-%s\n", num, "奇数")
+		}
+	}
+}
