0133dfec49a1ce902153094566ed3a849c5f3128
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2023-04-20 21:56:12 +0900

    built_in_type

---
+++ try/built_in_type.go
@@ -0,0 +1,13 @@
+package main
+
+// 型キャスト
+func main() {
+	var sum i
+	sum = 5 + 6 + 3
+	var avg float64
+	avg = float64(sum) / float64(3) // float64で統一する必要がある。
+
+	if avg > 4.5 {
+		println("%f", avg)
+	}
+}
