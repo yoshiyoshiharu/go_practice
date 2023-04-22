2141f921577f18cf27ebf7d673b6d99f242650fd
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2023-04-22 16:45:36 +0900

    study

--- study/func.go
+++ study/func.go
@@ -11,6 +11,15 @@ func main() {
 	// 複数返す関数のうち、不要な戻り値があるときは_を使う
 	_, x := addMinus(1, 2)
 	fmt.Println(x)
+
+	// 関数型
+	fs := make([]func() string, 2) // 関数を要素に持つスライス
+	fs[0] = func() string { return "hello" }
+	fs[1] = func() string { return "world" }
+
+	for _, f := range fs {
+		println(f())
+	}
 }
 
 func add(x int, y int) int {
