8e349916316d3d4cd35a33b1cb72e2f0d188d458
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2023-04-22 17:27:10 +0900

    method

---
+++ study/method.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Hex int
+// Hex型のメソッドを定義
+// 関数定義の前にレシーバを渡す
+func (h Hex) Tostring() string {
+	return fmt.Sprintf("%x", int(h))
+}
+
+func main() {
+	var hex Hex = 100
+	println(hex.Tostring())
+
+	// メソッドは値としても扱える
+	f := hex.Tostring
+	println(f())
+
+	// メソッド式
+	f2 := Hex.Tostring
+	println(f2(hex))
+}
---
+++ try/pointer_receiver.go
@@ -0,0 +1,13 @@
+package main
+
+type MyInt int
+func (n *MyInt) Inc() { *n++ }
+
+func main() {
+	var n MyInt
+	println(n)
+	n.Inc()
+	println(n)
+	n.Inc()
+	println(n)
+}
