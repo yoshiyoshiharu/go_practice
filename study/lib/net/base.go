51b1d130f5d6f0d3448f1479668fb546bcb3c723
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-05 15:32:49 +0900

    http study

---
+++ study/lib/net/base.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+)
+
+// localhost:8080にアクセスすると文字列が表示される
+func main() {
+	http.HandleFunc("/", handler) // ハンドラを登録(パターン, 関数)
+	http.ListenAndServe(":8080", nil) // HTTPサーバーの起動(ホスト名:ポート番号, HTTPハンドラ(nilのときは登録したハンドラ))
+}
+
+// httpハンドラ
+// 第1引数はレスポンスを書き込む先
+// 第2引数はクライアントからのリクエスト
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, HTTP server") // 書き込み先を指定して出力
+}
