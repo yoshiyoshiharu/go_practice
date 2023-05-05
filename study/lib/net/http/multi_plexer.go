a7a9116f26bfeb74302c647d4a5958531160d5aa
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-05 20:05:27 +0900

    multi_prexer

--- study/lib/net/http/multi_plexer.go
+++ study/lib/net/http/multi_plexer.go
@@ -5,4 +5,23 @@ import(
 )
 
 func main() {
+	personMux := http.NewServeMux()
+	personMux.HandleFunc("/greet",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("こんにちは"))
+		})
+
+	dogMux := http.NewServeMux()
+	dogMux.HandleFunc("/greet",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("かわいい子犬ちゃん"))
+		})
+
+	mux := http.NewServeMux()
+	// /personのパターンはpersonMuxが処理
+	mux.Handle("/person/", http.StripPrefix("/person", personMux))
+	// /dogのパターンはdogMuxが処理
+	mux.Handle("/dog/", http.StripPrefix("/dog", dogMux))
+
+	http.ListenAndServe(":8080", nil)
 }
