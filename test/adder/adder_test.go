7a13540042eaf09654ce12045942c92f36880938
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-06 10:40:11 +0900

    prepare

--- test/adder/adder_test.go
+++ test/adder/adder_test.go
@@ -1,10 +1,27 @@
 package adder
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"time"
+	"os"
+)
+
+var currentTime time.Time // パッケージブロックで宣言
+
+func TestMain(m *testing.M) {
+	fmt.Println("prepare test")
+	currentTime = time.Now()
+	exitVal := m.Run() // テストの実行
+	fmt.Println("cleanup test")
+	os.Exit(exitVal)
+}
 
 func Test_addNumbers(t *testing.T) {
 	result := addNumbers(2, 3)
 	if result != 5 {
 		t.Error("結果が違う: 期待する結果 5, 得られた結果", result)
 	}
+
+	fmt.Println(currentTime)
 }
